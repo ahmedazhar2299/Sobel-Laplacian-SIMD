4a15f2c42cfd68433e99937f4fd9fa86146edb8d
Muhammad Ahmed <ahmed.azhar2299@gmail.com>
2025-09-28 00:36:10 -0700

    update: code cleanup

--- imgproc.c
+++ imgproc.c
@@ -13,9 +13,6 @@
 
 Image *Laplacian(Image *);
 Image *Sobel(Image *);
-Image *Gamma(Image *, float ratio);
-Image *Global_histogram(Image *);
-Image *Local_histogram(Image *);
 Image *ReadPNMImage(char *);
 Image *CreateNewImage(Image *, char *comment);
 int boundaryCheck(int index_x, int index_y, int width, int height);
@@ -27,16 +24,12 @@ double now_seconds(void);
 
 int main(int argc, char **argv)
 {
-    // Please adjust the input filename and path to suit your needs:
-    // char* file_in = (char*)"lena.pgm";
-    // char* file_out = (char*)"";
     double start = now_seconds();
 
     char *input_dir = (argc > 1) ? argv[1] : "images";
     char *pgm_output_dir = (argc > 2) ? argv[2] : "grayscale_inputs_pgm";
 
     process_folder_to_pgm_then_run(input_dir, pgm_output_dir);
-    // TestReadImage(file_in, file_out);
 
     double end = now_seconds();
     printf("\n###############################\n");
@@ -102,18 +95,12 @@ void process_folder_to_pgm_then_run(char *input_dir, char *pgm_output_dir) {
 
     struct dirent *ent;
     while ((ent = readdir(dir)) != NULL) {
-        // skip . and ..
         if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) continue;
-
-        // Build full input path: <input_dir>/<name>
         char in_path[1024];
         snprintf(in_path, sizeof(in_path), "%s/%s", input_dir, ent->d_name);
-
-        // Convert to PGM
         char pgm_path[1024];
         if (convert_to_pgm(in_path, pgm_output_dir, pgm_path, sizeof(pgm_path)) == 0) {
             // Now run your pipeline on the generated PGM
-            // file_out is unused in your current TestReadImage; pass empty string
             TestReadImage(pgm_path, "");
         }
     }
@@ -125,31 +112,15 @@ int TestReadImage(char *file_in, char *file_out)
 {
     Image *image;
     Image *laplacian, *sobel;
-    // Image *sobel, *histogram_global, *histogram_local;
-    // Image *gama_01, *gama_04, *gama_07, *gama_1;
     char *stem = Extract_Filename_Stem(file_in);
     image = ReadPNMImage(file_in);
     laplacian = Laplacian(image);
-    sobel = Sobel(image);
-    // gama_01 = Gamma(image, 0.1);
-    // gama_04 = Gamma(image, 0.4);
-    // gama_07 = Gamma(image, 0.7);
-    // gama_1 = Gamma(image, 1);
-    // histogram_global = Global_histogram(image);
-    // histogram_local = Local_histogram(image);
-
-    // Please adjust the output filenames and paths to suit your needs:
+    // sobel = Sobel(image);
     char filename[300];
     snprintf(filename, sizeof(filename), "%s.pgm", stem);
 
     SavePNMImage(laplacian, filename, "laplacian_pgm", "laplacian_png");
-    SavePNMImage(sobel, filename, "sobel_pgm", "sobel_png");
-    // SavePNMImage(gama_01, (char*)"gama_01.pgm");
-    // SavePNMImage(gama_04, (char*)"gama_04.pgm");
-    // SavePNMImage(gama_07, (char*)"gama_07.pgm");
-    // SavePNMImage(gama_1, (char*)"gama_1.pgm");
-    // SavePNMImage(histogram_global, (char*)"histogram_global.pgm");
-    // SavePNMImage(histogram_local, (char*)"histogram_local.pgm");
+    // SavePNMImage(sobel, filename, "sobel_pgm", "sobel_png");
     free(stem);
     return(0);
 }
@@ -208,109 +179,6 @@ Image *Sobel(Image *image) {
     return (outimage);
 }
 
-Image *Gamma(Image *image, float ratio) {
-    unsigned char *tempin, *tempout;
-    float temp;
-    float variance, average, sum = 0, N = image->Width * image->Height; // calculate variance
-    
-    Image *outimage;
-    outimage = CreateNewImage(image, (char*)"#testing function");
-    tempin = image->data;
-    tempout = outimage->data;
-    
-    for(int i = 0; i < image->Height; i++) {
-        for(int j = 0; j < image->Width; j++) {
-            temp = ((float)tempin[image->Width * i + j] + 0.5) / 256; // normalized
-            temp = pow(temp, ratio); // power the parameter
-            temp = (int)(temp * 256 - 0.5); // denormalization
-            tempout[outimage->Width * i + j] = (unsigned char)temp;
-            sum += temp;
-        }
-    }
-    
-    // calculate & output the variance:
-    average = sum / N;
-    sum = 0;
-    for(int i = 0; i < image->Height; i++) {
-        for(int j = 0; j < image->Width; j++) {
-            sum += pow(tempout[outimage->Width * i + j] - average, 2);
-        }
-    }
-    variance = sum / N;
-    printf("The variance of gamma value %.1f is: %.2f\n", ratio, variance);
-    return (outimage);
-}
-
-Image *Global_histogram(Image *image) {
-    unsigned char *tempin, *tempout, temp;
-    int histogram_sum[256], histogram[256], currSum = 0; // used for statistics
-    float constant = (float)255 / (float)(image->Width * image->Height);// (L-1)/(M*N)
-    
-    Image *outimage;
-    outimage = CreateNewImage(image, (char*)"#testing function");
-    tempin = image->data;
-    tempout = outimage->data;
-    
-    // initialize the array:
-    for(int i = 0; i < 256; i++) histogram[i] = 0;
-    
-    for(int i = 0; i < image->Height; i++) {
-        for(int j = 0; j < image->Width; j++) {
-            temp = tempin[image->Width * i + j];
-            histogram[temp] += 1;
-        }
-    }
-    for(int i = 0; i < 256; i++) {
-        currSum += histogram[i];
-        histogram_sum[i] = currSum;
-    }
-    
-    // output the image:
-    for(int i = 0; i < image->Height; i++) {
-        for(int j = 0; j < image->Width; j++) {
-            temp = tempin[image->Width * i + j];
-            tempout[outimage->Width * i + j] = (int)(histogram_sum[temp] * constant);
-        }
-    }
-    return (outimage);
-}
-
-Image *Local_histogram(Image *image) {
-    unsigned char *tempin, *tempout, temp;
-    int histogram_sum[256], histogram[256]; // used for statistics
-    float constant = (float)255 / (float)9; // M*N changed to 9
-    
-    Image *outimage;
-    outimage = CreateNewImage(image, (char*)"#testing function");
-    tempin = image->data;
-    tempout = outimage->data;
-    
-    // process all the pixels:
-    for(int i = 0; i < image->Height; i++) {
-        for(int j = 0; j < image->Width; j++) {
-            // initialize the array:
-            for(int k = 0; k < 256; k++) histogram[k] = 0;
-
-            for(int x = -1; x <= 1; x++) {
-                for(int y = -1; y <= 1; y++) {
-                    // use boundary check:
-                    temp = boundaryCheck(j + y, i + x, image->Width, image->Height) ? tempin[image->Width * (i + x) + (j + y)] : 0;
-                    histogram[temp] += 1;
-                }
-            }
-            for(int k = 0, currSum = 0; k < 256; k++) {
-                currSum += histogram[k];
-                histogram_sum[k] = currSum;
-            }
-            
-            // output the image:
-            temp = tempin[image->Width * i + j];
-            tempout[outimage->Width * i + j] = (int)(histogram_sum[temp] * constant);
-        }
-    }
-    return (outimage);
-}
-
 int boundaryCheck(int index_x, int index_y, int width, int height) {
     if(index_x >= 0 && index_y >= 0 && index_x < width && index_y < height) return 1;
     else return 0;
@@ -349,9 +217,8 @@ Image *ReadPNMImage(char *filename)
         exit(0);
     }
     
-    if(ch == '5')image->Type=GRAY;  // Gray (pgm)
-    else if(ch == '6')image->Type=COLOR;  //Color (ppm)
-    /* skip comments */
+    if(ch == '5')image->Type=GRAY; 
+    else if(ch == '6')image->Type=COLOR;  
     ch = getc(fp);
     j=0;
     while (ch == '#')
@@ -362,11 +229,11 @@ Image *ReadPNMImage(char *filename)
             ch = getc(fp);
             image->comments[num_comment_lines][j]=ch;
             j++;
-        } while (ch != '\n');     /* read to the end of the line */
+        } while (ch != '\n'); 
         image->comments[num_comment_lines][j-1]='\0';
         j=0;
         num_comment_lines++;
-        ch = getc(fp);            /* thanks, Elliot */
+        ch = getc(fp);            
     }
     
     if (!isdigit((int)ch)){
@@ -374,17 +241,9 @@ Image *ReadPNMImage(char *filename)
         exit(0);
     }
     
-    ungetc(ch, fp);               /* put that digit back */
+    ungetc(ch, fp);       
     
-    /* read the width, height, and maximum value for a pixel */
     fscanf(fp, "%d%d%d\n", &Width, &Height, &maxval);
-    
-    /*
-     if (maxval != 255){
-     printf("image is not true-color (24 bit); read failed");
-     exit(0);
-     }
-     */
     
     if(image->Type == GRAY)
         size          = Width * Height;
@@ -401,35 +260,22 @@ Image *ReadPNMImage(char *filename)
     }
     
     num = fread((void *) image->data, 1, (size_t) size, fp);
-    //printf("Complete reading of %d bytes \n", num);
     if (num != size){
         printf("cannot read image data from file");
         exit(0);
     }
     
-    //for(j=0;j<image->num_comment_lines;j++){
-    //      printf("%s\n",image->comments[j]);
-    //      }
-    
     fclose(fp);
-    
-    /*-----  Debug  ------*/
     
     if(image->Type == GRAY)printf("..Image Type PGM\n");
     else printf("..Image Type PPM Color\n");
-    /*
-     printf("Width %d\n", Width);
-     printf("Height %d\n",Height);
-     printf("Size of image %d bytes\n",size);
-     printf("maxvalue %d\n", maxval);
-     */
     return(image);
 }
 
 char *Extract_Filename_Stem(char *file_in) {
     const char *fname = file_in;
     const char *slash1 = strrchr(file_in, '/');
-    const char *slash2 = strrchr(file_in, '\\'); // Windows support
+    const char *slash2 = strrchr(file_in, '\\'); 
     if (slash1 || slash2) {
         const char *slash = (slash1 > slash2 ? slash1 : slash2);
         fname = slash + 1;
@@ -450,16 +296,11 @@ void SavePNMImage(Image *temp_image, char *filename, char *pgm_directory_name, c
     int size;
     FILE *fp;
 
-    // Ensure output directories exist
     ensure_output_dir(pgm_directory_name);
     ensure_output_dir(png_directory_name);
-
-    // Build PGM full path: <pgm_directory_name>/<filename>
     char pgm_path[512];
     snprintf(pgm_path, sizeof(pgm_path), "%s/%s", pgm_directory_name, filename);
 
-    // Derive PNG filename from `filename`
-    // If filename ends with .pgm or .ppm -> replace with .png, else append .png
     const char *dot = strrchr(filename, '.');
     char png_filename[256];
     if (dot && (strcasecmp(dot, ".pgm") == 0 || strcasecmp(dot, ".ppm") == 0)) {
@@ -472,11 +313,9 @@ void SavePNMImage(Image *temp_image, char *filename, char *pgm_directory_name, c
         snprintf(png_filename, sizeof(png_filename), "%s.png", filename);
     }
 
-    // Build PNG full path: <png_directory_name>/<png_filename>
     char png_path[512];
     snprintf(png_path, sizeof(png_path), "%s/%s", png_directory_name, png_filename);
 
-    // ---- Write PNM (PGM/PPM) ----
     printf("Saving Image %s\n", pgm_path);
     fp = fopen(pgm_path, "wb");
     if (!fp) {
@@ -484,10 +323,10 @@ void SavePNMImage(Image *temp_image, char *filename, char *pgm_directory_name, c
         exit(1);
     }
 
-    if (temp_image->Type == GRAY) {  // Gray (pgm)
+    if (temp_image->Type == GRAY) { 
         fprintf(fp, "P5\n");
         size = temp_image->Width * temp_image->Height;
-    } else if (temp_image->Type == COLOR) {  // Color (ppm)
+    } else if (temp_image->Type == COLOR) { 
         fprintf(fp, "P6\n");
         size = temp_image->Width * temp_image->Height * 3;
     } else {
@@ -509,8 +348,6 @@ void SavePNMImage(Image *temp_image, char *filename, char *pgm_directory_name, c
     }
     fclose(fp);
 
-    // ---- Auto-convert PGM/PPM -> PNG via ImageMagick so you don't have to run it manually ----
-    // Uses `magick` CLI; ensure ImageMagick is installed. Quotes handle spaces in paths.
     char cmd[1200];
     snprintf(cmd, sizeof(cmd), "magick \"%s\" \"%s\"", pgm_path, png_path);
     int rc = system(cmd);
